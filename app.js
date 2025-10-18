af4782c50284b26d1e5d63e309dc6880c3a36d6f
Marcelo <marcelo.bracet@g.globo>
2025-10-18 00:37:59 -0300

    fix: Update server port configuration to use environment variable and correct log message

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ import express from "express";
 
 const app = express();
 
+const port = process.env.PORT || 3001;
+
 app.get("/", (_, res) => {
   res.send("🔥 Hello World from Marcelo's VPS!");
 });
 
-app.listen(3001, "0.0.0.0", () => {
-  console.log("Server running on port 3000");
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on port ${port}`);
 });
